212b998ef145a066dcf0cab6a7c6b3cee0090584
Herick Borgo <hericoborgo1@gmail.com>
2022-07-06 23:53:48 -0300

    Adicionado axios

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,13 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  lintOnSave: 'error',
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'https://604540c7c0194f00170bcb53.mockapi.io',
+        changeOrigin: true,
+      },
+    },
+  },
 });
